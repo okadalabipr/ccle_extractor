29d468d53fc72e5b173ebda225f74525c6b1bd88
himoto <31299606+himoto@users.noreply.github.com>
2020-07-15 16:08:24 +0900

    Add metabolomics data

--- README.md
+++ README.md
@@ -10,36 +10,55 @@ Generating test and/or training datasets from [CCLE](https://portals.broadinstit
 - CCLE_RNAseq_rsem_genes_tpm_20180929.txt.gz
 - Cell_lines_annotations_20181226.txt
 - CCLE_RNAseq_genes_counts_20180929.gct.gz
+- CCLE_metabolomics_20190502.csv
 
 ## Usage
-```python
-from ccle_processing import CancerCellLineEncyclopedia as CCLE
+1. Gene expression data
+    ```python
+    from ccle_processing import CancerCellLineEncyclopedia as CCLE
 
-# Set gene_nemes
-# Set ccle_names or cell_lines
+    # Set gene_nemes
+    # Set ccle_names or cell_lines
 
-selected_CCLE_subset = CCLE(
-    gene_names = ['EGFR', 'ERBB2', 'ERBB3', 'ERBB4'],
-    ccle_names = ['MCF7_BREAST', 'MDAMB231_BREAST']
-)
+    selected_CCLE_subset = CCLE(
+        gene_names = ['EGFR', 'ERBB2', 'ERBB3', 'ERBB4'],
+        ccle_names = ['MCF7_BREAST', 'MDAMB231_BREAST']
+    )
 
-''' or
-selected_CCLE_subset = CCLE(
-    gene_names = ['EGFR', 'ERBB2', 'ERBB3', 'ERBB4'],
-    cell_lines = ['MCF7', 'MDA-MB-231']
-)
-'''
+    ''' or
+    selected_CCLE_subset = CCLE(
+        gene_names = ['EGFR', 'ERBB2', 'ERBB3', 'ERBB4'],
+        cell_lines = ['MCF7', 'MDA-MB-231']
+    )
+    '''
 
-# GeneCards link (https://www.genecards.org)
-selected_CCLE_subset.to_gene_summary()
-# TPM value
-selected_CCLE_subset.to_expression()
-```
+    # GeneCards link (https://www.genecards.org)
+    selected_CCLE_subset.to_gene_summary()
+    # TPM value
+    selected_CCLE_subset.to_gene_expression()
+    ```
+1. Metabolomics dataset
+    ```python
+    from ccle_processing import CancerCellLineEncyclopedia as CCLE
+
+    # Set metabolite_nemes
+    # Set ccle_names or cell_lines
+
+    selected_CCLE_subset = CCLE(
+        metabolite_names = ['lactate', 'glutamine', 'glutamate', 'serine', 'alanine'],
+        ccle_names = ['MCF7_BREAST', 'MDAMB231_BREAST']
+    )
+
+    # Metabolite levels
+    selected_CCLE_subset.to_metabolomics()
+    ```
 
 ## Output
 - gene_summary.md
 - tpm_values.csv
-- expression/
+- gene_expression/
+- metabolite_levels.csv
+- metabolomics/
 
 ## Installation
     $ git clone https://github.com/okadalabipr/CCLEctor.git
--- ccle_processing.py
+++ ccle_processing.py
@@ -10,8 +10,15 @@ from cycler import cycler
 class CancerCellLineEncyclopedia(object):
     """https://portals.broadinstitute.org/ccle
     """
-    def __init__(self, gene_names, cell_lines=[], ccle_names=[]):
+    def __init__(
+            self,
+            gene_names=[],
+            metabolite_names=[],
+            cell_lines=[],
+            ccle_names=[],
+    ):
         self.gene_names = gene_names
+        self.metabolite_names = metabolite_names
         self.cell_lines = cell_lines
         self.ccle_names = ccle_names
         
@@ -29,10 +36,16 @@ class CancerCellLineEncyclopedia(object):
             'CCLE_RNAseq_genes_counts_20180929.gct.gz',
             header=2, usecols=range(2)
         )
+        self.metabolomics_data = pd.read_csv(
+            'https://data.broadinstitute.org/ccle/'\
+            'CCLE_metabolomics_20190502.csv',
+            index_col=0
+        )
 
         self.annotations_name = set(self.annotations.Name)
         self.annotations_ccle_id = set(self.annotations.CCLE_ID)
         self.counts_description = set(self.counts.Description)
+        self.all_metabolites = set(self.metabolomics_data.columns[1:])
 
     def _gene2id(self, gene):
         if gene not in self.counts_description:
@@ -89,7 +102,7 @@ class CancerCellLineEncyclopedia(object):
             ccle_ids.append(self._cell2id(cell))
         return ccle_ids
 
-    def _extract(self):
+    def _extract_tpm(self):
         gene_ids = self._get_gene_id()
         ccle_ids = self._get_ccle_id() \
             if len(self.ccle_names) < len(self.cell_lines) else self.ccle_names
@@ -102,24 +115,32 @@ class CancerCellLineEncyclopedia(object):
         )
         data.to_csv('tpm_values.csv')
         return data
+    
+    def _extract_metabolite_level(self):
+        not_exist = []
+        for i, metabolite in enumerate(self.metabolite_names):
+            if metabolite not in self.all_metabolites:
+                print("metabolite '{}' does not exist.\n".format(metabolite))
+                not_exist.append(i)
+        for i in sorted(not_exist, reverse=True):
+            self.metabolite_names.pop(i)
+        ccle_ids = self._get_ccle_id() \
+            if len(self.ccle_names) < len(self.cell_lines) else self.ccle_names
+        data = self.metabolomics_data.loc[ccle_ids, self.metabolite_names]
+        data.index.name = None
+        data.rename(
+            index=lambda x: self._id2cell(x),
+            inplace=True
+        )
+        data.to_csv('metabolite_levels.csv')
+        return data
 
-    def to_expression(self):
+    def to_gene_expression(self):
         if not self.cell_lines and not self.ccle_names:
             raise ValueError('cell_lines or ccle_names must be filled in.')
-        os.makedirs('./expression', exist_ok=True)
-        data = self._extract()
-        # rcParams
-        plt.rcParams['font.family'] = 'Arial'
-        plt.rcParams['font.size'] = 28
-        plt.rcParams['axes.linewidth'] = 2
-        plt.rcParams['xtick.major.width'] = 2
-        plt.rcParams['ytick.major.width'] = 2
-        plt.rcParams['axes.prop_cycle'] = cycler(
-            color=[
-                '#4E79A7', '#F28E2B', '#E15759', '#76B7B2','#59A14E',
-                '#EDC949','#B07AA2','#FF9DA7','#9C755F','#BAB0AC'
-            ]
-        )
+        os.makedirs('./gene_expression', exist_ok=True)
+        data = self._extract_tpm()
+        self._set_rcParams()
         for gene in self.gene_names:
             if gene in self.counts_description:
                 ax = data.loc[gene].plot.bar(
@@ -130,7 +151,7 @@ class CancerCellLineEncyclopedia(object):
                 ax.set_ylabel('TPM')
                 sns.despine()
                 plt.savefig(
-                    './expression/{}.pdf'.format(gene), bbox_inches='tight'
+                    './gene_expression/{}.pdf'.format(gene), bbox_inches='tight'
                 )
                 plt.close()
     
@@ -148,3 +169,36 @@ class CancerCellLineEncyclopedia(object):
                     f.write(
                         '|'+gene+'|'+gene_id+'|'+gene_cards_url+'|\n'
                     )
+
+    def to_metabolomics(self):
+        if not self.cell_lines and not self.ccle_names:
+            raise ValueError('cell_lines or ccle_names must be filled in.')
+        os.makedirs('./metabolomics', exist_ok=True)
+        data = self._extract_metabolite_level()
+        self._set_rcParams()
+        for metabolite in self.metabolite_names:
+            ax = data.loc[:, metabolite].plot.bar(
+                figsize=(
+                    2*max(len(self.cell_lines), len(self.ccle_names)), 6
+                ), fontsize=28, title=metabolite,
+            )
+            ax.set_ylabel('Metabolite levels')
+            sns.despine()
+            plt.savefig(
+                './metabolomics/{}.pdf'.format(metabolite), bbox_inches='tight'
+            )
+            plt.close()
+
+    @staticmethod
+    def _set_rcParams():
+        plt.rcParams['font.family'] = 'Arial'
+        plt.rcParams['font.size'] = 28
+        plt.rcParams['axes.linewidth'] = 2
+        plt.rcParams['xtick.major.width'] = 2
+        plt.rcParams['ytick.major.width'] = 2
+        plt.rcParams['axes.prop_cycle'] = cycler(
+            color=[
+                '#4E79A7', '#F28E2B', '#E15759', '#76B7B2','#59A14E',
+                '#EDC949','#B07AA2','#FF9DA7','#9C755F','#BAB0AC'
+            ]
+        )
